b0cd7bf739990759e02fa1ad2245a0e54cec24d7
Lee Presswood <pressedlee123@gmail.com>
2015-03-09 11:13:59 -0500

    Added project.

---
+++ app/desktop/src/com/jacl/capstone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jacl.capstone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jacl.capstone.CapstoneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CapstoneGame(), config);
+	}
+}
