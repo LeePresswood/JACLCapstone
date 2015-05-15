f2ae5b6eae474548bda62d5eb3bc052bd9cc4502
Lee Presswood <pressedlee123@gmail.com>
2015-05-15 00:06:13 -0500

    Moved to Version 0.2 Alpha.
    Goals for this verson:
    -Enemies
    -Items
    -Events

--- app/core/src/com/jacl/capstone/data/Constants.java
+++ app/core/src/com/jacl/capstone/data/Constants.java
@@ -10,7 +10,7 @@ public class Constants
 {
 	//All values should be static members to this class to allow retrieval without instantiation.
 	public static final String GAME_NAME = "Game";		//Use this to write the name of the game in the UI box.
-	public static final String GAME_VERSION = "0.1";	/* Use this to determine the version of the game in V.U format. 
+	public static final String GAME_VERSION = "0.2";	/* Use this to determine the version of the game in V.U format. 
 																		 * Example: Version 1 - Update 4 would be version "1.4"
 																		 * Increment the Update number frequently (weekly?) and the Version
 																		 * number only when a major feature addition has been made.
