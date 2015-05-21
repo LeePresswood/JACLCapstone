7600545713e66a6d9d3529530b9c800ff0b1db24
Lee Presswood <pressedlee123@gmail.com>
2015-05-21 10:16:09 -0500

    Added damage calculating class.

---
+++ app/core/src/com/jacl/capstone/helpers/DamageCalculator.java
@@ -0,0 +1,31 @@
+package com.jacl.capstone.world.entities.npc.enemies.bosses;
+
+import com.jacl.capstone.world.World;
+import com.jacl.capstone.helpers;
+
+/**
+* Calculate the damage an entity takes based upon the amount
+* being affected by defense.
+* 
+* @author Lee
+*
+*/
+public class DamageCalculator
+{
+	/**
+	 * To make damage calculations affected by defense, we
+	 * must lower the damage by a certain percentage per
+	 * point of defense the given entity has.
+	 */
+	private final float PERCENT_PER_DEFENSE_POINT = 2f;
+	
+	/**
+	 * Damage is directly lowered by amount of defense player
+	 * has.
+	 */
+	public static float getDamage(float damage, float defense)
+	{
+		float total = damage * (100f - defense * PERCENT_PER_DEFENSE_POINT) / 100f;
+		return total < 0f ? 0f : total;
+	}
+}
