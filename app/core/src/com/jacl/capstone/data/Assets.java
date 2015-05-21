230864b41d5b3f295c3f99e628100f41724b50f6
Lee Presswood <pressedlee123@gmail.com>
2015-05-21 10:49:29 -0500

    Clarified Javadoc.

--- app/core/src/com/jacl/capstone/data/Assets.java
+++ app/core/src/com/jacl/capstone/data/Assets.java
@@ -6,8 +6,14 @@ import com.badlogic.gdx.graphics.Texture;
 /**
  * This class should be used to load our assets.<br><br>
  * 
- * To retrieve them:<br>
+ * To load assets:<br>
  * load(<file_name>, <asset_type>.class);
+ * 
+ * To retrieve them:<br>
+ * get(<file_name>, <asset_type>.class);
+ * 
+ * Various String constants will be added to allow
+ * for calling of file names in a cleaner fashion.
  * 
  * @author Lee
  *
