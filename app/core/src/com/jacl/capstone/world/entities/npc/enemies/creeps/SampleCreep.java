3affadd5e619d32aaecb028fc5af18c2b5919e88
Lee Presswood <pressedlee123@gmail.com>
2015-05-21 12:01:28 -0500

    Added sample enemy.

--- app/core/src/com/jacl/capstone/world/entities/npc/enemies/creeps/SampleCreep.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/enemies/creeps/SampleCreep.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.jacl.capstone.world.World;
 import com.jacl.capstone.world.entities.npc.enemies.Enemy;
 
+/**
+ * This enemy will not be in the final game. Purely for testing.
+ */
 public class SampleCreep extends Enemy
 {
 
@@ -16,7 +19,9 @@ public class SampleCreep extends Enemy
 	@Override
 	protected Sprite makeSprite(float x, float y)
 	{
-		return null;
+		Sprite s = new Sprite(world.screen.game.assets.get(Assets.PLAYER, Texture.class);
+		s.setBounds(x, y, 1f * world.map_handler.tile_size, 1f * world.map_handler.tile_size);
+		return s;
 	}
 
 	@Override
