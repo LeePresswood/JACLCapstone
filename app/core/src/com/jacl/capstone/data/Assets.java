f8807e27f2c8510831df06d1f8fc2c729e7b1335
Lee Presswood <pressedlee123@gmail.com>
2015-05-21 10:51:14 -0500

    Added first file name constant.

--- app/core/src/com/jacl/capstone/data/Assets.java
+++ app/core/src/com/jacl/capstone/data/Assets.java
@@ -7,10 +7,10 @@ import com.badlogic.gdx.graphics.Texture;
  * This class should be used to load our assets.<br><br>
  * 
  * To load assets:<br>
- * load(<file_name>, <asset_type>.class);
+ * load(<file_name>, <asset_type>.class);<br><br>
  * 
  * To retrieve them:<br>
- * get(<file_name>, <asset_type>.class);
+ * get(<file_name>, <asset_type>.class);<br><br>
  * 
  * Various String constants will be added to allow
  * for calling of file names in a cleaner fashion.
@@ -20,6 +20,8 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public class Assets extends AssetManager
 {
+	public static final String PLAYER = "image.png";
+	
 	public Assets()
 	{
 		//Audio
