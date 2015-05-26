a4ec13f013818e9b86943bb90e77aa2961f0090b
Lee Presswood <pressedlee123@gmail.com>
2015-05-25 22:25:58 -0500

    Reduced number of calculations in Knockbacker

--- app/core/src/com/jacl/capstone/helpers/Knockbacker.java
+++ app/core/src/com/jacl/capstone/helpers/Knockbacker.java
@@ -1,7 +1,6 @@
 package com.jacl.capstone.helpers;
 
 import com.jacl.capstone.data.enums.Direction;
-import com.jacl.capstone.world.World;
 import com.jacl.capstone.world.entities.MovingEntity;
 
 /**
@@ -12,34 +11,33 @@ import com.jacl.capstone.world.entities.MovingEntity;
  */
 public class Knockbacker
 {
-	private World world;
 	private MovingEntity entity;
 	
 	//Knockback should always be the same amount for continuity throughout the game regardless of entity.
 	private final float KNOCKBACK_BLOCKS = 1.5f;
 	private final float KNOCKBACK_SPEED = 20f;
+	private final float KNOCKBACK_SPEED_TICK;
 	private final float KNOCKBACK_DISTANCE;
 	
 	//Knockback itself will have a flag set if happening. It also has distance and direction.
-	public boolean is_being_knocked_back;
 	public float current_knockback;
 	public Direction knockback_direction;
+	public boolean is_being_knocked_back;
 	
 	public Knockbacker(MovingEntity entity)
 	{
-		this.world = entity.world;
 		this.entity = entity;
 		
-		//Knockback block distance is knockback_blocks * size of blocks.
-		KNOCKBACK_DISTANCE = KNOCKBACK_BLOCKS * world.map_handler.tile_size;
+		//Transform to correct units (pixels to blocks).
+		KNOCKBACK_SPEED_TICK = KNOCKBACK_SPEED * entity.world.map_handler.tile_size;
+		KNOCKBACK_DISTANCE = KNOCKBACK_BLOCKS * entity.world.map_handler.tile_size;
 	}
 	
 	/**
-	 * The type of knockback that hit causes. The direction is set from an earlier collision calculation.
+	 * The type of knockback that being hit causes. The direction is set from an earlier collision calculation.
 	 */
 	public void doKnockback()
 	{
-		//Knockback is dependent upon the direction the entity is facing. If no movement happens before being hit, no direction is set.
 		is_being_knocked_back = true;
 		current_knockback = 0f;
 	}
@@ -59,17 +57,17 @@ public class Knockbacker
 		if(is_being_knocked_back)
 		{
 			//Calculate the knockback.
-			current_knockback += delta * KNOCKBACK_SPEED * world.map_handler.tile_size;
+			current_knockback += delta * KNOCKBACK_SPEED_TICK;
 			if(knockback_direction == Direction.LEFT)
-				entity.sprite.translateX(delta * KNOCKBACK_SPEED * world.map_handler.tile_size);
+				entity.sprite.translateX(delta * KNOCKBACK_SPEED_TICK);
 			else if(knockback_direction == Direction.RIGHT)
-				entity.sprite.translateX(-delta * KNOCKBACK_SPEED * world.map_handler.tile_size);
+				entity.sprite.translateX(-delta * KNOCKBACK_SPEED_TICK);
 			else if(knockback_direction == Direction.UP)
-				entity.sprite.translateY(-delta * KNOCKBACK_SPEED * world.map_handler.tile_size);
-			else if(knockback_direction == Direction.DOWN)
-				entity.sprite.translateY(delta * KNOCKBACK_SPEED * world.map_handler.tile_size);
+				entity.sprite.translateY(-delta * KNOCKBACK_SPEED_TICK);
+			else
+				entity.sprite.translateY(delta * KNOCKBACK_SPEED_TICK);
 			
-			//If necessary, end.
+			//If knockback is over, end.
 			if(current_knockback >= KNOCKBACK_DISTANCE)
 			{
 				is_being_knocked_back = false;
