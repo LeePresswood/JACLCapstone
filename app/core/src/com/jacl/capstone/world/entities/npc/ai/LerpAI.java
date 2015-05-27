6aacc9e7abb6a707c3bc340d0f2a5e898d7427dd
Lee Presswood <LeePresswood@gmail.com>
2015-05-27 08:37:24 -0500

    Added really bad lerping

--- app/core/src/com/jacl/capstone/world/entities/npc/ai/LerpAI.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/ai/LerpAI.java
@@ -1,6 +1,5 @@
 package com.jacl.capstone.world.entities.npc.ai;
 
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.jacl.capstone.world.entities.npc.NPC;
 
@@ -13,31 +12,52 @@ import com.jacl.capstone.world.entities.npc.NPC;
 public class LerpAI extends AI
 {
 	//There will be a radius around the AI unit where the unit is close enough to attack the player.
-	private final float ATTACK_RADIUS = 0.5f;
+	private final float ATTACK_RADIUS;
+	private Vector2 my_position;
 	private Vector2 player_position;
 	
 	public LerpAI(NPC npc)
 	{
 		super(npc);
+		
+		ATTACK_RADIUS = (float) Math.pow(1.5f * npc.world.map_handler.tile_size, 2);
+		
+		my_position = new Vector2();
 		player_position = new Vector2();
 	}
 
 	@Override
 	public void updateThinking(float delta)
 	{
-		System.out.println("Looking at player at position " + handler.player.getCenterX() + "," + handler.player.getCenterY());
+		//Get entity's position (referencing the center).
+		my_position.x = npc.getCenterX();
+		my_position.y = npc.getCenterY();
+		
+		//Get player's position (referencing the center).
+		player_position.x = handler.player.getCenterX();
+		player_position.y = handler.player.getCenterY();
 	}
 
 	@Override
 	public void updatePosition(float delta)
 	{
-		System.out.println("Moving step.");
+		//Move toward player's center by reference of this entity's center.
+		my_position.lerp(player_position, 0.5f);
+		npc.sprite.setPosition(my_position.x, my_position.y);
 	}
 
 	@Override
 	public void updateAction(float delta)
 	{
-		//If we're within the attack radius of the player, attack.
+		/* If we're within the attack radius of the player, attack.
+		 * Note: We're using the squared distance between the two
+		 * entities to avoid having to do a square root operation.
+		 * Square roots are costly for a computer to calculate,
+		 * and we don't need an exact answer due to the nature of 
+		 * the project. The only thing we have to be aware of is
+		 * the fact that, because our distance is squared, our
+		 * attack radius must also be squared to compensate. 
+		 */
 		if(Vector2.dst2(npc.getCenterX(), npc.getCenterY(), handler.player.getCenterX(), handler.player.getCenterY()) <= ATTACK_RADIUS)
 			System.out.println("Can attack.");
 		else
--- app/core/src/com/jacl/capstone/world/entities/npc/enemies/creeps/SampleCreep.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/enemies/creeps/SampleCreep.java
@@ -7,7 +7,7 @@ import com.jacl.capstone.data.Assets;
 import com.jacl.capstone.data.enums.Alignment;
 import com.jacl.capstone.world.World;
 import com.jacl.capstone.world.entities.npc.ai.AI;
-import com.jacl.capstone.world.entities.npc.ai.NothingAI;
+import com.jacl.capstone.world.entities.npc.ai.LerpAI;
 import com.jacl.capstone.world.entities.npc.enemies.Enemy;
 
 /**
@@ -31,7 +31,7 @@ public class SampleCreep extends Enemy
 	@Override
 	protected AI initAI()
 	{
-		return new NothingAI(this);
+		return new LerpAI(this);
 	}
 
 	@Override
