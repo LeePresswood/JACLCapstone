0713d0d9f4f290e54b17729ee65c29d550ed418f
Lee Presswood <pressedlee123@gmail.com>
2015-09-03 18:34:33 -0500

    Added what will eventually be pathfinding AI.

---
+++ app/core/src/com/jacl/capstone/world/entities/npc/ai/PathfindingAI.java
@@ -0,0 +1,28 @@
+package com.jacl.capstone.world.entities.npc.ai;
+
+import com.jacl.capstone.world.entities.npc.NPC;
+
+public class PathfindingAI extends AI
+{
+	
+	public PathfindingAI(NPC npc)
+	{
+		super(npc);
+	}
+
+	@Override
+	public void updateThinking(float delta)
+	{
+	}
+	
+	@Override
+	public void updatePosition(float delta)
+	{
+	}
+	
+	@Override
+	public void updateAction(float delta)
+	{
+	}
+	
+}
