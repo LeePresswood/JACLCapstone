b5c4be630f16833fd4f1d1de1031e254066769a1
Lee Presswood <LeePresswood@gmail.com>
2015-09-18 09:46:01 -0500

    Javadoc.

---
+++ app/core/src/com/jacl/capstone/hud/world/DialogueBox.java
@@ -0,0 +1,15 @@
+package com.jacl.capstone.hud.world;
+
+/**
+ * This class handles the text within a dialogue box. This text is calculated by being given the full dialogue and
+ * the index of the text. Break this text into multiple textboxes. 
+ * @author Lee
+ */
+public class DialogueBox
+{
+	
+	public DialogueBox()
+	{
+	}
+	
+}
