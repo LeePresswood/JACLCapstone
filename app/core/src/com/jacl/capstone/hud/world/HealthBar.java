5244342fef6c2adce233c4b2880e04d7b2dcad6c
Lee Presswood <LeePresswood@gmail.com>
2015-09-21 09:20:41 -0500

    Documentation.

--- app/core/src/com/jacl/capstone/hud/world/HealthBar.java
+++ app/core/src/com/jacl/capstone/hud/world/HealthBar.java
@@ -5,6 +5,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.jacl.capstone.hud.HUD;
 
+/**
+ * Represents the player's current health in a visual sense. Note: Don't use this
+ * visual representation when checking health-related functions (like dying or healing).
+ * Instead, we want to have a changeHealth() method in the Player class. This method
+ * will call the changeValue() method in this class.
+ * @author Lee
+ */
 public class HealthBar
 {
 	public HUD hud;
@@ -25,9 +32,18 @@ public class HealthBar
 		HealthBar = new NinePatch(new Texture(Gdx.files.internal("health-blue.png")),5,5,2,2);
 	}
 	
-	public void update(float delta)
+	public void changeCurrentValue(float new_value)
 	{
 		
+	}
+	
+	public void changeMaxValue(float new_max)
+	{
+		
+	}
+	
+	public void update(float delta)
+	{//Use this for animation.
 	}
 	
 	public void draw()
