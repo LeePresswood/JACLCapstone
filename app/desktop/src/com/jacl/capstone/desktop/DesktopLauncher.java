afe8cc77c67e2592000fc3f52c9d8977c58fb493
Lee Presswood <LeePresswood@gmail.com>
2015-09-21 10:58:37 -0500

    Updated to Version 0.4 Alpha

--- app/desktop/src/com/jacl/capstone/desktop/DesktopLauncher.java
+++ app/desktop/src/com/jacl/capstone/desktop/DesktopLauncher.java
@@ -24,13 +24,11 @@ public class DesktopLauncher
 	 * Until the game is out of testing stages, "Alpha" or "Beta" will 
 	 * be appended to the version number.
 	 * 
-	 * Goals for 0.3:
+	 * Goals for 0.4:
 	 * Use of items
-	 * Healthbars
 	 * Death of enemies and the player
-	 * Pathfinding AI.
 	 */
-	public static final String GAME_VERSION = "0.3 Alpha";
+	public static final String GAME_VERSION = "0.4 Alpha";
 	
 	/**
 	 * Game width will be a constant. Game height will be directly
