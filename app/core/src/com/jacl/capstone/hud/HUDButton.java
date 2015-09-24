bbb68e4fcdf3225de54f69d4430f48f852c4d6e1
Lee Presswood <LeePresswood@gmail.com>
2015-09-24 08:36:14 -0500

    Created HUDButton.

--- app/core/src/com/jacl/capstone/hud/HUDButton.java
+++ app/core/src/com/jacl/capstone/hud/HUDButton.java
@@ -1,10 +1,28 @@
 package com.jacl.capstone.hud;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.jacl.capstone.screens.ScreenParent;
+
 public class HUDButton
 {
+	public ScreenParent screen;
+	public Sprite sprite;
 	
-	public HUDButton()
+	public HUDButton(ScreenParent screen, float x, float y, float width, float height, Texture texture)
 	{
+		this.screen = screen;
+		
+		sprite = new Sprite(texture);
+		sprite.setBounds(x, y, width, height);
 	}
+
+	public void downColor()
+   {
+   }
+
+	public void click()
+   {
+   }
 	
 }
