0e0a423dff794e8bc37efa924ac7e8f09c9423ba
Lee Presswood <LeePresswood@gmail.com>
2015-09-28 09:14:28 -0500

    Defined RandomAI.

--- app/core/src/com/jacl/capstone/world/entities/npc/ai/RandomAI.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/ai/RandomAI.java
@@ -1,14 +1,33 @@
 package com.jacl.capstone.world.entities.npc.ai;
 
+import java.util.Random;
+
 import com.badlogic.gdx.math.Vector2;
 import com.jacl.capstone.world.entities.npc.NPC;
 
+/**
+ * Think of this AI as having a movement pattern that is the same as what you'd see from NPCs in Pokemon.
+ * Actions include either move or rest for movement and attack for special cases.
+ * @author Lee
+ */
 public class RandomAI extends AI
 {
 	//There will be a radius around the AI unit where the unit is close enough
 	//to attack the player.
 	private final float ATTACK_RADIUS;
 	private Vector2 player_position;
+	
+	//Two movement actions are either move or wait. Waiting should be the more common action of the two.
+	private Random random;
+	private final float CHANCE_WAIT = 0.7f;
+	
+	private boolean moving;
+	private float move_current;
+	private final float MOVE_MAX = 1f;
+	
+	private boolean waiting;
+	private float wait_current;
+	private final float wait_time = 0.75f;
 	
 	public RandomAI(NPC npc)
 	{
@@ -21,6 +40,7 @@ public class RandomAI extends AI
 	@Override
 	public void updateThinking(float delta)
 	{
+		
 	}
 	
 	@Override
