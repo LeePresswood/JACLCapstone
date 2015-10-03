4c5f3ceecaf0fc2d86359b852a7d4ec1a07b2f4b
Lee Presswood <pressedlee123@gmail.com>
2015-10-03 17:12:48 -0500

    Dialogue shows up on screen.

--- app/core/src/com/jacl/capstone/hud/world/DialogueBox.java
+++ app/core/src/com/jacl/capstone/hud/world/DialogueBox.java
@@ -29,7 +29,7 @@ public class DialogueBox
 	{
 		this.hud = hud;
 		
-		font = hud.screen.game.assets.get(Assets.FONT24, BitmapFont.class);
+		font = hud.screen.game.assets.get(Assets.FONT16, BitmapFont.class);
 		lines = text.split(NEW_LINE_SPLIT);
 	}
 	
