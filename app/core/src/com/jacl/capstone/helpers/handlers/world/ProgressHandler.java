ff540b09afdc6ebc3d63e7d25aee9f8bd8f5206a
Lee Presswood <LeePresswood@gmail.com>
2015-10-12 09:43:09 -0500

    Created ProgressHandler.

---
+++ app/core/src/com/jacl/capstone/helpers/handlers/world/ProgressHandler.java
@@ -0,0 +1,14 @@
+package com.jacl.capstone.helpers.handlers.world;
+
+/**
+ * Handles the progress of the game.
+ * @author Lee
+ */
+public class ProgressHandler
+{
+	
+	public ProgressHandler()
+	{
+	}
+	
+}
