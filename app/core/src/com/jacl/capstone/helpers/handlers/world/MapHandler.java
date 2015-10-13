1ffd212a36192be793b796390fbf6c7f07298033
Lee Presswood <LeePresswood@gmail.com>
2015-10-13 08:51:40 -0500

    Added chapter in save fi

--- app/core/src/com/jacl/capstone/helpers/handlers/world/MapHandler.java
+++ app/core/src/com/jacl/capstone/helpers/handlers/world/MapHandler.java
@@ -47,11 +47,12 @@ public class MapHandler
 	/**
 	 * Initialize portions of the map.
 	 */
-	public void handlerInit(String map_name)
+	public void handlerInit(String map_name, int map_chapter)
 	{
-		//Get map data.
+		//Get map file path.
+		String chapter_directory = "Chapter" + map_chapter + "/";
 		this.map_name = map_name;
-		map = new TmxMapLoader().load(MAP_DIRECTORY + map_name);
+		map = new TmxMapLoader().load(MAP_DIRECTORY + chapter_directory + map_name);
 		tiled_map_renderer = new OrthogonalTiledMapRenderer(map);
 		
 		//Read bounds and sizes of map.
