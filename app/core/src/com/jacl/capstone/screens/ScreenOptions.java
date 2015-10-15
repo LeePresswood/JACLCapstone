778ce6fc36581210852e2559e9cd10523ff35496
Lee Presswood <LeePresswood@gmail.com>
2015-10-15 08:26:52 -0500

    Added slider example for Huy.

--- app/core/src/com/jacl/capstone/screens/ScreenOptions.java
+++ app/core/src/com/jacl/capstone/screens/ScreenOptions.java
@@ -15,16 +15,18 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider.SliderStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-
 import com.jacl.capstone.CapstoneGame;
 
-public class ScreenOptions extends ScreenAdapter{
-
-	public ScreenOptions(CapstoneGame game) {
-		this.game=game;
+public class ScreenOptions extends ScreenAdapter
+{
+	
+	public ScreenOptions(CapstoneGame game)
+	{
+		this.game = game;
 		this.last_screen = game.getScreen();
 	}
 	
@@ -38,29 +40,30 @@ public class ScreenOptions extends ScreenAdapter{
 	private BitmapFont bitmap;
 	private Label heading;
 	private Label volumeValue;
-
+	
 	@Override
 	/**
 	 * This is called when the screen is created.
 	 * show settings in game
 	 * putting sliders and checkbox for sound and graphic
 	 */
-	public void show() {
+	public void show()
+	{
 		stage = new Stage();
 		
 		Gdx.input.setInputProcessor(stage);
 		
 		atlas = new TextureAtlas("atlas.pack");
 		skin = new Skin(atlas);
-	
-		//TableLayout layout = table.getTableLayout();
 		
-		//fonts
+		// TableLayout layout = table.getTableLayout();
+		
+		// fonts
 		table = new Table(skin);
 		table.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		bitmap = new BitmapFont(Gdx.files.internal("hud/fonts/font44.fnt"),false);
+		bitmap = new BitmapFont(Gdx.files.internal("hud/fonts/font44.fnt"), false);
 		
-		//buttons styles
+		// buttons styles
 		TextButtonStyle textButtonStyle = new TextButtonStyle();
 		textButtonStyle.up = skin.getDrawable("button.up");
 		textButtonStyle.down = skin.getDrawable("button.down");
@@ -68,84 +71,93 @@ public class ScreenOptions extends ScreenAdapter{
 		textButtonStyle.pressedOffsetY = -1;
 		textButtonStyle.font = bitmap;
 		
-		//back button which is back to main menu
+		// back button which is back to main menu
 		buttonBack = new TextButton("Back", textButtonStyle);
-		buttonBack.addListener(new ClickListener(){
+		buttonBack.addListener(new ClickListener()
+		{
 			@Override
-			public void clicked(InputEvent event, float x, float y){
+			public void clicked(InputEvent event, float x, float y)
+			{
 				((CapstoneGame) Gdx.app.getApplicationListener()).setScreen(last_screen);
 			}
 		});
 		buttonBack.pad(5);
 		
-		
-		//heading
+		// heading
 		String label = "Options";
 		LabelStyle headingStyle = new LabelStyle(bitmap, Color.WHITE);
-		heading = new Label(label,headingStyle);
+		heading = new Label(label, headingStyle);
 		heading.setFontScale(2);
 		
 		/*
-		//create sounds option
-		final CheckBox soundEffectsCheckbox = new CheckBox("Sound Effects", skin);
-		soundEffectsCheckbox.setChecked(game.getPreferences().isSoundEffectsEnabled() );
-		soundEffectsCheckbox.addListener(new ClickListener() {
-			public void clicked(InputEvent event, float x, float y)
-			{
-				boolean enabled = soundEffectsCheckbox.isChecked();
-				game.getPreferences().setSoundEffectsEnabled(enabled);
-			}
-		});
-		*/
-		/*
-		//create music option
-		final CheckBox musicEffectsCheckbox = new CheckBox("Music Effects", skin);
-		musicEffectsCheckbox.setChecked(game.getPreferences().isMusicEffectsEnabled() );
-		musicEffectsCheckbox.addListener(new ClickListener() {
-			public void clicked(InputEvent event, float x, float y)
-			{
-				boolean enabled = musicEffectsCheckbox.isChecked();
-				game.getPreferences().setMusicEffectsEnabled(enabled);
-			}
-		});
-		*/
-		//range is [0.0,1.0]; step is 0.1f
-		/** 
-		 * slider doesn't work correctly
+		 * //create sounds option final CheckBox soundEffectsCheckbox = new
+		 * CheckBox("Sound Effects", skin);
+		 * soundEffectsCheckbox.setChecked(game.getPreferences
+		 * ().isSoundEffectsEnabled() ); soundEffectsCheckbox.addListener(new
+		 * ClickListener() { public void clicked(InputEvent event, float x, float
+		 * y) { boolean enabled = soundEffectsCheckbox.isChecked();
+		 * game.getPreferences().setSoundEffectsEnabled(enabled); } });
 		 */
-		//final Slider volumeSlider = new Slider(0.1f,4, 0.1f,false,skin);
-		//volumeSlider.setValue(1);
-		//volumeSlider.getCaptureListeners();
-		//volumeValue = new Label("volume", skin);
-		//updateVolumeLabel();
+		/*
+		 * //create music option final CheckBox musicEffectsCheckbox = new
+		 * CheckBox("Music Effects", skin);
+		 * musicEffectsCheckbox.setChecked(game.getPreferences
+		 * ().isMusicEffectsEnabled() ); musicEffectsCheckbox.addListener(new
+		 * ClickListener() { public void clicked(InputEvent event, float x, float
+		 * y) { boolean enabled = musicEffectsCheckbox.isChecked();
+		 * game.getPreferences().setMusicEffectsEnabled(enabled); } });
+		 */
+		// range is [0.0,1.0]; step is 0.1f
+		/**
+		 * slider doesn't work correctly
+		 * 
+		 * Lee's edit: I looked at your Slider, and it looks like your problem is
+		 * with the skin you're using. To fix that, you'll need a SliderStyle.
+		 * This will allow you to set the background of the slider bar and the
+		 * little knob that the user moves. I made an example in the code. 
+		 * 
+		 * Also, the volumeValue label is broken for the same reason. The name "default"
+		 * is not defined in the skin you're using. You'll need to make a LabelStyle to fix this.
+		 */
+		SliderStyle style = new SliderStyle();
+		style.background = skin.getDrawable("button.up");
+		style.knob = skin.getDrawable("button.down");
 		
-		//add together stuffs
+		final Slider volumeSlider = new Slider(0.1f, 4, 0.1f, false, style);
+		volumeSlider.setValue(1);
+		volumeSlider.getCaptureListeners();
+		// volumeValue = new Label("volume", skin);
+		updateVolumeLabel();
+		
+		// add together stuffs
 		table.add(heading);
 		table.row();
 		table.getCell(heading).spaceBottom(70);
-		//table.add(volumeSlider);
-		//table.add(volumeValue);
-		//table.add(musicEffectsCheckbox);
-		//table.row();
-		//table.add(soundEffectsCheckbox);
-		//table.row();
+		table.add(volumeSlider);
+		// table.add(volumeValue);
+		// table.add(musicEffectsCheckbox);
+		// table.row();
+		// table.add(soundEffectsCheckbox);
+		// table.row();
 		table.add(buttonBack);
 		table.row();
 		stage.addActor(table);
 	}
-
+	
 	@Override
-	public void render(float delta) {
+	public void render(float delta)
+	{
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stage.act(delta);
 		stage.draw();
-	
+		
 	}
-
+	
 	@Override
-	public void dispose() {
+	public void dispose()
+	{
 		stage.dispose();
 		atlas.dispose();
 		skin.dispose();
@@ -154,7 +166,7 @@ public class ScreenOptions extends ScreenAdapter{
 	
 	private void updateVolumeLabel()
 	{
-		//float volume=(game.getPreferences().getVolume()*100);
-		//volumeValue.setText(String.format((Locale.US, "%0.1f%%",volume));
+		// float volume=(game.getPreferences().getVolume()*100);
+		// volumeValue.setText(String.format((Locale.US, "%0.1f%%",volume));
 	}
 }
