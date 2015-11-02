2bd643911a6e69b5629c6c4759ae4f010e707484
Lee Presswood <pressedlee123@gmail.com>
2015-11-01 22:47:48 -0600

    Added substance to RangedEnemy

--- app/core/src/com/jacl/capstone/world/entities/npc/enemies/RangedEnemy.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/enemies/RangedEnemy.java
@@ -1,6 +1,28 @@
 package com.jacl.capstone.world.entities.npc.enemies;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.XmlReader.Element;
+import com.jacl.capstone.data.enums.Alignment;
+import com.jacl.capstone.world.World;
+import com.jacl.capstone.world.entities.npc.ai.AI;
+
 public class RangedEnemy extends Enemy
 {
+
+	public RangedEnemy(World world, float x, float y, Element data, Alignment alignment)
+	{
+		super(world, x, y, data, alignment);
+	}
+
+	@Override
+	protected AI initAI()
+	{
+		return null;
+	}
+
+	@Override
+	public void draw(SpriteBatch batch)
+	{
+	}
 	
 }
