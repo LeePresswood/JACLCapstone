c14819bd21a3598225751a1d626bd83234727307
Lee Presswood <pressedlee123@gmail.com>
2015-11-02 00:21:32 -0600

    Added AI.

--- app/core/src/com/jacl/capstone/world/entities/npc/enemies/RangedEnemy.java
+++ app/core/src/com/jacl/capstone/world/entities/npc/enemies/RangedEnemy.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.utils.XmlReader.Element;
 import com.jacl.capstone.data.enums.Alignment;
 import com.jacl.capstone.world.World;
 import com.jacl.capstone.world.entities.npc.ai.AI;
+import com.jacl.capstone.world.entities.npc.ai.RandomAI;
 
 public class RangedEnemy extends Enemy
 {
@@ -12,12 +13,14 @@ public class RangedEnemy extends Enemy
 	public RangedEnemy(World world, float x, float y, Element data, Alignment alignment)
 	{
 		super(world, x, y, data, alignment);
+		
+		//On top of the enemy's sprite, this enemy will also have 
 	}
 
 	@Override
 	protected AI initAI()
 	{
-		return null;
+		return new RandomAI(this);
 	}
 
 	@Override
